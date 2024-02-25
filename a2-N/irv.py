182b5cffe667b1a219563ddc4b2f4bdc15bdd289
Ali Sartaz Khan <alisartazkhan@gmail.com>
2024-02-25 15:41:29 -0700

    Done w Week 1

--- a2-N/irv.py
+++ a2-N/irv.py
@@ -4,16 +4,14 @@ from typing import Dict, Hashable, List
 from common.types import Ballot, Result, Scheme
 from common.shared_main import shared_main
 
-
 def irv(ballots: List[Ballot]) -> Result:
+    candidates: set[Hashable] = set(candidate for ballot in ballots for candidate in ballot.ranking)
     total_votes: int = sum(ballot.tally for ballot in ballots)
     majority: float = total_votes / 2
     round: int = 1
-    candidate_not_found = True
-    eliminated_candidates: List[Hashable] = []
-    while candidate_not_found:
+    eliminated_candidates: set[Hashable] = set()
+    while True:
         first_choice_votes: Dict[Hashable, int] = {}
-
         # counting first choice votes
         for ballot in ballots:
             if len(ballot.ranking) != 0:  
@@ -22,38 +20,42 @@ def irv(ballots: List[Ballot]) -> Result:
                         continue
                     if ballot.ranking[i] not in first_choice_votes:
                         first_choice_votes[ballot.ranking[i]] = 0
-                    # print("ADDINGS {} points to {}".format(ballot.tally, ballot.ranking[i]))
                     first_choice_votes[ballot.ranking[i]] += ballot.tally
                     break
-
-        print("MAJORITY: ",majority,  "| FCV: ", first_choice_votes)
-
+        
+        missing_cadidates: set[Hashable] = candidates ^ first_choice_votes.keys()
+        # print("CANDIDATES: ", candidates, "| MAJORITY: ",majority,  "| FCV: ", first_choice_votes, "ROUND:", round)
 
         # Check for a majority winner
         for candidate, votes in first_choice_votes.items():
             if votes > majority:
                 return candidate, True  # True indicates a single winner
-            if len(first_choice_votes) == 1:
+            if len(first_choice_votes.keys()) == 1:
                 return candidate, True
+        
+        if len(missing_cadidates) > 1 and round == 1:
+            return -2, False
+        
+        if round == 1:
+            for c in missing_cadidates:
+                first_choice_votes[c] = 0
+            
+        # return None, False
+
+       
             
         count: int = 0
+
         for candidate, votes in first_choice_votes.items():
             if votes == min(first_choice_votes.values()):
-                eliminated_candidates.append(candidate)
+                eliminated_candidates.add(candidate)
                 count += 1
+        
+
         if count > 1:
             return None, False
-        # print("ELIM CAND: ", eliminated_candidates)
-        # If all remaining candidates have the same number of votes, it's a tie
-        # if len(first_choice_votes) == len(eliminated_candidates):
-        #     return -2, False  # False indicates a tie
-
-        # Eliminate the candidate(s) and remove them from the ballots
-        # for ballot in ballots:
-        #     ballot.ranking = [candidate for candidate in ballot.ranking if candidate not in eliminated_candidates]
-
+        
         round += 1
-    return -3, False
 
 scheme: Scheme = irv
 name: str = "irv"
@@ -61,17 +63,16 @@ name: str = "irv"
 
 
 def main() -> None:
-    # shared_main("irv", scheme)
-    # ballot1 = Ballot((1,2), 2)
-    # ballot2 = Ballot((2,0), 0)
-    # ballot3 = Ballot((2,1), 11)
+    shared_main("irv", scheme)
 
-    ballots = [Ballot(ranking=(4, 3), tally=4),
-    Ballot(ranking=(0, 2), tally=3),
-    Ballot(ranking=(4, 1), tally=2),
-    Ballot(ranking=(3, 0), tally=4)]
-    result, no_ties = irv(ballots)
-    print(f"Winner: {result}, No ties: {no_ties}")
+
+    # ballots = [   Ballot(ranking=(2, 1), tally=2),
+    # Ballot(ranking=(3, 4), tally=3),
+    # Ballot(ranking=(0, 3), tally=0),
+    # Ballot(ranking=(4, 2), tally=5),
+    # Ballot(ranking=(3, 0), tally=3)]
+    # result, no_ties = irv(ballots)
+    # print(f"Winner: {result}, No ties: {no_ties}")
 
 
 if __name__ == "__main__":
